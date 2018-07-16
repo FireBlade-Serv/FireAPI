7d5a85f095ac9636e22e6926b00c6e59fc4ffc6a
Glowstoner <glowstoner1@gmail.com>
2018-07-16 02:48:44 +0200

    v4.0.1: Patch EncryptionKet not Serializable

--- src/fr/glowstoner/fireapi/crypto/EncryptionKey.java
+++ src/fr/glowstoner/fireapi/crypto/EncryptionKey.java
@@ -1,10 +1,14 @@
 package fr.glowstoner.fireapi.crypto;
 
+import java.io.Serializable;
+
 import lombok.Data;
 
 @Data
-public class EncryptionKey {
+public class EncryptionKey implements Serializable{
 
+	private static final long serialVersionUID = -8185224456640740822L;
+	
 	private String key;
 	
 	public EncryptionKey(String key) {
